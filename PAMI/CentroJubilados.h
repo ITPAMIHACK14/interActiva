728837d4ebdd926cc9219b5bffcadfb4464fd996
estefaniamiguel <est.miguel@gmail.com>
2014-11-17 20:49:07 -0300

    subiendo los archivos del hackathon

---
+++ PAMI/CentroJubilados.h
@@ -0,0 +1,13 @@
+//
+//  CentroJubilados.h
+//  PAMI
+//
+//  Created by Estefania Soledad Miguel on 15/11/14.
+//  Copyright (c) 2014 EstefaniaMiguel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CentroJubilados : NSObject
+
+@end
---
+++ PAMI/CentroJubilados.m
@@ -0,0 +1,13 @@
+//
+//  CentroJubilados.m
+//  PAMI
+//
+//  Created by Estefania Soledad Miguel on 15/11/14.
+//  Copyright (c) 2014 EstefaniaMiguel. All rights reserved.
+//
+
+#import "CentroJubilados.h"
+
+@implementation CentroJubilados
+
+@end
